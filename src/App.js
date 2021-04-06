ca3151c71d4b987e162de2dbc1dbdcb634f072a8
Sibongumusa Lungelo <81442054+S-codes14@users.noreply.github.com>
2021-04-06 11:16:46 +0200

    added new uri
    there was a failure to connect to localhost

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
 
     if (!hashParams.access_token) {
       window.location.href =
-        'https://accounts.spotify.com/authorize?client_id=230be2f46909426b8b80cac36446b52a&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback';
+        'https://accounts.spotify.com/authorize?client_id=e8c4fe82e12b4f2c842e9d8459636506&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=https://s-codes14.github.io/30sekify/callback';
     } else {
       this.props.setToken(hashParams.access_token);
     }
